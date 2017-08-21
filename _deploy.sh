af5050e1c1541e97af864fb6222ca5b3680bfd59
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-08-21 17:08:33 +0100

    Only deploy book if built from master branch

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 
-# configure your name and email if you have not done so
+# Configure your name and email if you have not done so
+
 git config --global user.email ${EMAIL}
 git config --global user.name ${USERNAME}
 
-# clone the repository to the book-output directory
+# Ensure that the book will only be updated when the build is 
+# triggered from the master branch.
+
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+# Clone the repository to the book-output directory
+
 git clone -b gh-pages \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
   book-output
+
+# Copy locally built *.html files into 
+
+cp *.html book-output
+
+# Add the locally built files to a commit and push
+
 cd book-output
-cp -r ../_book/* ./
-git add *
-git commit -m "Update the book"
+git add *.html -f
+git commit -m "Automatic build update" || true
 git push origin gh-pages
